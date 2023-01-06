50f627e9c10d9725542a46a31df63f24f87caecb
Akshay Manthekar <58980814+Akshay4452@users.noreply.github.com>
2023-01-06 12:58:04 +0530

    Random Color Generated
    Player Random Color is generated out of 4 colors that we used viz. Magenta/Pink/Cyan/Yellow. At time of game start, player will spawn with random color out of these 4 choices

--- Color_Switch/Assets/Scripts/Player.cs
+++ Color_Switch/Assets/Scripts/Player.cs
@@ -3,19 +3,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float jumpForce = 10f;
+    public string CurrentCol;
+    public SpriteRenderer spriteRenderer;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
 
     Rigidbody2D rb;
     void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
+        SetRandomColor();
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0,3);
+        switch (index)
+        {
+            case 0:
+                CurrentCol = "Cyan";
+                spriteRenderer.color = colorCyan;
+                break;
+            case 1:
+                CurrentCol = "Yellow";
+                spriteRenderer.color = colorYellow;
+                break;
+            case 2:
+                CurrentCol = "Magenta";
+                spriteRenderer.color = colorMagenta;
+                break;
+            case 3:
+                CurrentCol = "Pink";
+                spriteRenderer.color = colorPink;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Jump") || Input.GetMouseButtonDown(0))
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpForce;
         }
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        
     }
 }
